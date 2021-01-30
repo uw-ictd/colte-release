7b7ba95d6f78355848be1e86104d1d60607c54ff
Matt Johnson <matt9j@cs.washington.edu>
2021-01-29 18:14:55 -0800

    docker: Pass the distro release version to the build
    The distro release version needs to be passed to the build to
    influence the final generated deb version.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,19 +3,26 @@
 # A docker entrypoint script to build a package
 set -e
 
-make all
+# Assign input parameters to more descriptive variables
+
+# Distro version is distinct from codename to ensure the distro version always
+# compares newer as new versions are released. For example, debian9 < debian10,
+# while stretch (debian9's codename) is not < buster (debian10's).
+DISTRO_RELEASE_VERSION=$1
+DISTRO_RELEASE_CODENAME=$2
+OUT_USER=$3
+
+# Buildthe package
+DISTRO_RELEASE_VERSION=$DISTRO_RELEASE_VERSION make all
 
 # Copy the built packages to the mounted output volume
-OUT_DIR=$1
-OUT_USER=$2
-
-mkdir -p /build-volume/"${OUT_DIR}"
-mv BUILD/*.deb /build-volume/"${OUT_DIR}"
+mkdir -p /build-volume/"${DISTRO_RELEASE_CODENAME}"
+mv BUILD/*.deb /build-volume/"${DISTRO_RELEASE_CODENAME}"
 
 if test -z "$OUT_USER"
 then
     echo "No output user set, leaving as container user (likely root)"
 else
-    chown --recursive $OUT_USER /build-volume/"${OUT_DIR}"
-    chgrp --recursive $OUT_USER /build-volume/"${OUT_DIR}"
+    chown --recursive $OUT_USER /build-volume/"${DISTRO_RELEASE_CODENAME}"
+    chgrp --recursive $OUT_USER /build-volume/"${DISTRO_RELEASE_CODENAME}"
 fi
